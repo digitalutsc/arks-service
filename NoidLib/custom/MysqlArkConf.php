ed4074375bf11c5fe940959d5c000ab7f9d422d3
Kyle Huynh <kyle.huynh@utoronto.ca>
2020-10-21 14:59:44 +0000

    empty mysql credentials file

--- NoidLib/custom/MysqlArkConf.php
+++ NoidLib/custom/MysqlArkConf.php
@@ -11,10 +11,10 @@
 namespace Noid\Lib\Custom;
 
 class MysqlArkConf{
-    static public $mysql_host = 'localhost';
-    static public $mysql_user = 'root';
-    static public $mysql_passwd = '0o9i8u7y';
-    static public $mysql_dbname = 'ARK_CORE';
+    static public $mysql_host = '';
+    static public $mysql_user = '';
+    static public $mysql_passwd = '';
+    static public $mysql_dbname = '';
     static public $mysql_port = 3306;
 //	static public $mysql_socket = '';
 }
