89f14514507dc6a16258b381371f53a571a1d39c
Kyle Huynh <kyle.huynh@utoronto.ca>
2021-03-31 14:37:05 -0400

    added errors pages

---
+++ 403.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+  <!-- Basic Page Needs
+  ================================================== -->
+  <meta charset="utf-8">
+  <title>University of Toronto Scarborough ARK ID Service</title>
+  <meta name="description" content="University of Toronto Scarborough ARK ID Service provided by Digital Scholarship Unit at University of Toronto Scarborough">
+  <meta name="keywords" content="University of Toronto Scarborough ARK ID Service provided by Digital Scholarship Unit at University of Toronto Scarborough">
+
+  <script src="https://www.google.com/recaptcha/api.js"></script>
+
+  <meta name="author" content="">
+  <!-- Mobile Specific Metas
+  ================================================== -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta http-equiv="x-ua-compatible" content="IE=9">
+  <!-- Font Awesome -->
+  <link href="front/stylesheets/font-awesome.css" rel="stylesheet">
+  <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+  <!--[if lt IE 9]>
+  <script src="js/html5shiv.js"></script>
+  <script src="js/respond.min.js"></script>
+  <![endif]-->
+  <!--headerIncludes-->
+  <!-- CSS
+  ================================================== -->
+  <link rel="stylesheet" href="front/stylesheets/menu.css">
+  <link rel="stylesheet" href="front/stylesheets/flat-ui-slider.css">
+  <link rel="stylesheet" href="front/stylesheets/base.css">
+  <link rel="stylesheet" href="front/stylesheets/skeleton.css">
+  <link rel="stylesheet" href="front/stylesheets/landings.css">
+  <link rel="stylesheet" href="front/stylesheets/main.css">
+  <link rel="stylesheet" href="front/stylesheets/landings_layouts.css">
+  <link rel="stylesheet" href="front/stylesheets/lightbox.css">
+  <link rel="stylesheet" href="front/stylesheets/pixicon.css">
+  <link href="front/assets/css/animations.min.css" rel="stylesheet" type="text/css" media="all" />
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+  <!-- Favicons OLD
+  ==================================================
+  <link rel="shortcut icon" href="front/images/favicon.ico">
+  <link rel="apple-touch-icon" href="front/images/apple-touch-icon.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="front/images/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="front/images/apple-touch-icon-114x114.png">
+  -->
+  <link rel="apple-touch-icon" sizes="57x57" href="front/images/dsu-favicon/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="front/images/dsu-favicon/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="front/images/dsu-favicon/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="front/images/dsu-favicon/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="front/images/dsu-favicon/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="front/images/dsu-favicon/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="front/images/dsu-favicon/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="front/images/dsu-favicon/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="front/images/dsu-favicon/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="front/images/dsu-favicon/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="front/images/dsu-favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="front/images/dsu-favicon/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="front/images/dsu-favicon/favicon-16x16.png">
+  <link rel="manifest" href="front/images/dsu-favicon/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="front/images/dsu-favicon/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+
+</head>
+<body>
+
+<div id="page" class="page">
+
+  <div class="pixfort_normal_1" id="section_header_1_dark">
+    <div class="header_style header_nav_1 dark pix_builder_bg">
+      <div class="container">
+        <div class="sixteen columns firas2">
+          <nav role="navigation" class="navbar navbar-white navbar-embossed navbar-lg pix_nav_1">
+            <div class="containerss">
+              <div class="navbar-header">
+                <img src="front/images/uploads/utsc-library-logo.png" class="pix_nav_logo" alt="" style="border-radius: 0px; border-color: rgb(255, 255, 255); border-style: none; border-width: 1px; width: 347px; height: 88px;">
+              </div>
+            </div><!-- /.container -->
+
+          </nav>
+        </div>
+      </div><!-- container -->
+    </div>
+  </div>
+  <div class="pixfort_normal_1 creative_video_header" id="section_pix_4">
+    <div class="has-video creative_header">
+      <div class="mask"></div>
+      <div class="pix_video_div section_video">
+        <video autoplay="" poster="front/images/video/video_bg.jpg" class="bgvid" loop="">
+          <!--              <source src="front/images/video/video_bg.webm" type="video/webm">
+               <source src="front/images/video/video_bg.mp4" type="video/mp4">
+               <source src="front/images/video/video_bg.ogv" type="video/ogv">
+           </video> -->
+      </div>
+      <div class="section_wrapper clearfix">git 
+        <div class="container">
+          <div class="sixteen columns context_style">
+            <div class="title_style">
+              <span class="editContent" style=""><span class="pix_text">Access Denied</span></span>
+            </div>
+            <div class="subtitle_style">
+                                <span class="creative_h_text">
+                                    <span class="editContent" style=""><span class="pix_text">You have landed on the home page for UTSC Library's ARK ID resolver</span></span>
+                                </span>
+              <a href="https://collections.digital.utsc.utoronto.ca" class="pix_button btn_normal bold_text red_bg " name="fff" data-selector=".pix_button" style="">
+                <span data-selector="Footers" style="">Visit Digital Scholarship Collections</span></a>
+
+            </div>
+          </div>
+        </div><!-- container -->
+      </div>
+    </div>
+  </div>
+
+  <div class="section_pointer" pix-name="video"></div><div class="dark" id="section_features_2_dark">
+    <div class="big_padding pix_builder_bg" style="outline-offset: -3px;">
+      <div class="container">
+        <div class="sixteen columns">
+          <div class="four columns onethird_style alpha">
+            <div class="f1_box">
+              <div class="big_icon light_blue">
+                <span class="pi pixicon-video2"></span>
+              </div>
+              <div class="center_text">
+                <span class="small_title bold_text"><span class="editContent" style=""><span class="pix_text">Media Objects</span></span></span><br>
+                <span class="editContent light_gray" style=""><span class="pix_text"> </span></span>
+              </div>
+            </div>
+          </div>
+          <div class="four columns onethird_style alpha">
+            <div class="f1_box">
+              <div class="big_icon light_blue">
+                <span class="pi pixicon-map2"></span>
+              </div>
+              <div class="center_text">
+                <span class="small_title bold_text"><span class="editContent" style=""><span class="pix_text">Relevant to UTSC</span></span></span><br>
+                <span class="editContent light_gray" style=""><span class="pix_text"> </span></span>
+              </div>
+            </div>
+          </div>
+          <div class="four columns onethird_style alpha">
+            <div class="f1_box">
+              <div class="big_icon light_blue">
+                <span class="pi pixicon-layers2"></span>
+              </div>
+              <div class="center_text">
+                <span class="small_title bold_text"><span class="editContent" style=""><span class="pix_text">Organized Carefully</span></span></span><br>
+                <span class="editContent light_gray" style=""><span class="pix_text"> </span></span>
+              </div>
+            </div>
+          </div>
+          <div class="four columns onethird_style alpha">
+            <div class="f1_box">
+              <div class="big_icon light_blue">
+                <span class="pi pixicon-tools"></span>
+              </div>
+              <div class="center_text">
+                <span class="small_title bold_text"><span class="editContent" style=""><span class="pix_text">Continually Reviewed</span></span></span><br>
+                <span class="editContent light_gray" style=""><span class="pix_text"> </span></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="normal_padding pix_builder_bg dark event_bg" id="section_normal_4_1" style="outline-offset: -3px;">
+  <div class="container ">
+    <div class="sixteen columns">
+      <div class="intro_div">
+        <p class="editContent" style="outline: none; cursor: default;">The University of Toronto Scarborough Library's Digital Scholarship Unit (DSU) provides UTSC-specific support for digital scholarship in the areas of data, the digital humanities, digital collections building, digital archiving, and scholarly communications.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+</div>
+
+<!-- JavaScript
+================================================== -->
+<script src="front/js-files/jquery-3.3.1.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+  jQuery.browser = {};
+  (function () {
+    jQuery.browser.msie = false;
+    jQuery.browser.version = 0;
+    if (navigator.userAgent.match(/MSIE ([0-9]+)./)) {
+      jQuery.browser.msie = true;
+      jQuery.browser.version = RegExp.$1;
+    }
+  })();
+</script>
+<script src="front/js-files/jquery.easing.1.3.js" type="text/javascript"></script>
+<script type="text/javascript" src="front/js-files/jquery.common.min.js"></script>
+<script src="front/js-files/ticker.js" type="text/javascript"></script>
+<script src="front/js-files/custom1.js" type="text/javascript"></script>
+
+<script src="front/assets/js/appear.min.js" type="text/javascript"></script>
+<script src="front/js-files/jquery.ui.touch-punch.min.js"></script>
+<script src="front/js-files/bootstrap.min.js"></script>
+<script src="front/js-files/bootstrap-switch.js"></script>
+<script src="front/js-files/custom3.js" type="text/javascript"></script>
+
+
+<script src="front/assets/js/appear.min.js" type="text/javascript"></script>
+<script src="front/assets/js/animations.js" type="text/javascript"></script>
+</body>
+</html>
