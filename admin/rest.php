fe726f0c85703f5d78509ff6aefae9a3ce5c6209
joshdentremont <joshdentremont@users.noreply.github.com>
2025-01-23 11:55:07 -0400

    Update rest.php

--- admin/rest.php
+++ admin/rest.php
@@ -474,7 +474,7 @@ function selectBound()
   // sql which gets all bound arks 
   $sql_boundarks = "SELECT id, REPLACE (id, '$firstpart', '') as _id
       from (
-          SELECT DISTINCT SUBSTRING_INDEX(_key, '\t', 1) AS id
+          SELECT DISTINCT REGEXP_SUBSTR(_key, '".$firstpart."[[:digit:][:alpha:]]+(\\t[/.]*.*\\t)*') AS id
               FROM `<table-name>`
               WHERE _key LIKE '$firstpart%' 
               AND (_key NOT REGEXP '\\\\s:\\/c' AND _key NOT REGEXP '\\\\s:\\/h') 
